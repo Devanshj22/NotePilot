24a198bd2aa192ea39e4d9299554ba3964a1452a
Harveer00 <dhillonharveer@yahoo.com>
2025-06-21 17:45:06 -0400

    Changed the requirments.txt to requirements.txt

--- frontend/notepilot-react-app/src/App.js
+++ frontend/notepilot-react-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Upload from './Upload';
+import Auth from "./Auth";
 
 function App() {
   return (
     <div className="App" style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
       <h1>📘 NotePilot</h1>
       <p>Upload your PDF notes, slides, or textbooks — and get instant AI-generated summaries.</p>
+      <Auth />
       <Upload />
     </div>
   );
